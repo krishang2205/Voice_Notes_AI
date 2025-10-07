7f413f845d9972efa0febe54fd219f840a1005b5
Krishang Darji <krishangdarji@gmail.com>
2025-10-07 10:00:00 +0530

    feat(electron): add scaffold for backend process management

--- electron/main.js
+++ electron/main.js
@@ -26,16 +26,33 @@ function createWindow() {
     ipcMain.handle('window-is-maximized', () => mainWindow.isMaximized());
 
     // In dev, load localhost. In prod, load index.html
-    // For now, we assume dev mode on localhost:5173 (Vite default)
     const devUrl = 'http://localhost:5173';
-
     mainWindow.loadURL(devUrl).catch(() => {
         console.log('Waiting for frontend to start...');
         setTimeout(() => mainWindow.loadURL(devUrl), 3000);
     });
 }
 
+// Backend Management
+const { spawn } = require('child_process');
+let backendProcess = null;
+
+function startBackend() {
+    const backendPath = path.join(__dirname, '../backend'); // Adjust for prod later
+    console.log('Starting backend service...');
+
+    // In dev, we might already have it running, but for packaging:
+    // backendProcess = spawn('node', ['dist/server.js'], { cwd: backendPath });
+    // For now, we'll just log that we would start it here in production
+    // or if the user wants to run "all-in-one".
+
+    // Simplistic dev implementation:
+    // This is just a placeholder to show WHERE we would spawn it.
+    // In real dev, we run `npm run dev:backend` separately.
+}
+
 app.whenReady().then(() => {
+    startBackend();
     createWindow();
 
     app.on('activate', () => {
@@ -47,6 +64,9 @@ app.whenReady().then(() => {
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
+        if (backendProcess) {
+            backendProcess.kill();
+        }
         app.quit();
     }
 });
